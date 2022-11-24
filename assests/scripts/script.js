08f67a2e93bbea4ce9de0a7d6e0dcae12f3f6205
Chukeze <enile12@gmail.com>
2022-11-23 18:18:21 -0600

    Fix pen color change and grid sizing
    Refactored the code so that the pen color is updated when the user
    changes the color of the color picker.
    Grid sizing was change removing separation of concerns
    Done by using the Array.from to create grid of any size. and use a event
    listener to update the size based on the newly selected size.
    create x amount of.

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,8 +1,9 @@
-let square;
+/*let square;
 const board = document.querySelector('.board');
 const boardScreen = document.createElement('div');
 boardScreen.classList.add("screen");
 board.appendChild(boardScreen);
+
 function dimensions(a,b){
     return a * b;
 }
@@ -22,25 +23,71 @@ function createGrid() {
     }
     /*square = document.querySelector('.square');
     let findSquareWidth = square.offsetWidth;
-    let findSquareHeight = square.offsetHeight;*/
+    let findSquareHeight = square.offsetHeight;
     boardScreen.style.gridTemplateColumns =  `repeat(${Math.sqrt(size) - 1 }, ${screenWidth}px) 1fr`;
     boardScreen.style.gridTemplateRows = `repeat(${Math.sqrt(size) - 1 }, ${screenWidth }px) 1fr `;
     
 //`grid-template: repeat(${(((Math.sqrt(size) / 2),(findBoardHeight.height % screenWidth))/((Math.sqrt(size) / 2),(findBoardHeight.height % screenWidth)))}px) 1fr 1fr 1fr
     /*boardScreen.setAttribute('style', 
         `grid-template-columns: repeat(${size},1fr)`
-    );*/
+    );
     //boardScreen.setAttribute('style', `grid-template-rows: repeat(${size}, 1fr)`,`grid-template-columns: repeat(${size}, 1fr)`);
 }
 
 function fillGrid(){
     createGrid();
+    
     square = document.querySelectorAll('.square');
     square.forEach((unit) =>{
         unit.addEventListener('mouseover', ()=>{
-            unit.style.backgroundColor = "black";
+            unit.style.backgroundColor = changePenColor();
+            //console.log(changePenColor())
         })
     })
 }
 
+function changePenColor(){
+    let newPenVal;
+    const pen = document.getElementById('color-picker');
+    pen.addEventListener('change', () =>{
+        newPenVal = pen;
+        console.log(newPenVal);
+    });
+    return pen;
+}
+
 fillGrid();
+
+*/
+/*const create = (tag = "div", options = {}, children = []) => {
+    const node = Object.assign(document.createElement(tag), options);
+    if(children.length) node.append(...children);
+    return node;
+}*/
+let penColor = "#000000";
+let boardSize = 16;
+const board = document.querySelector('.board');
+
+const createGrid = (length = 2) => {
+    [...board.children].forEach(el => el.remove());
+    board.style.setProperty("--repeat", length);
+    board.append(
+      ...Array.from({length: length ** 2}, () => document.createElement("div"))
+    );
+}
+
+document.querySelector('#board-size').addEventListener('change', (e) =>{
+    boardSize = e.target.value;
+    createGrid(boardSize);
+});
+
+document.querySelector('#color-picker').addEventListener('change', (e) => {
+  penColor = e.target.value;
+}, {passive: true});
+
+document.querySelector(".board").addEventListener("mouseover", e => {
+  if(!e.target.matches(".board > *")) return;
+  e.target.style.setProperty("--bg-color", penColor);
+}, {passive: true})
+
+createGrid();
